79bee0733775d56aefd667e155564990e36dd749
kr1sha <elustov@mail.ru>
2023-02-15 00:59:02 +0300

    добил

--- src/api/apk.js
+++ src/api/apk.js
@@ -1,1 +1,18 @@
-console.log("Hello World!")
+import Axios from 'axios'
+
+const axios = Axios.create({
+    baseURL: 'http//localhost:5000',
+})
+export default class API {
+    static async getMessage(id) {
+        const resp = await axios.get('/api/Messanger/${di}')
+        return resp.data
+    }
+
+    static async sendMessage(UserName, MessageText) {
+        await axios.post('/api/Messanger', {
+            MessageText,
+            UserName,
+        })
+    }
+}
